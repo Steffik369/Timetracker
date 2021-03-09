f92053e80a45b301e45251ed335434cd75e5685b
Steff <steffik369@gmail.com>
2021-03-09 10:20:53 +0100

    feat: Add ViewModel
    Added viewmodel interface and Timetracker viewmodel

---
+++ Timetracker/ViewModels/ITimetrackerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Timetracker.Models;
+
+namespace Timetracker.ViewModels
+{
+    interface ITimetrackerViewModel
+    {
+        bool IsBusy { get; }
+        List<JobItem> JobItems { get; }
+
+        JobItem JobItem { get; set; }
+
+        event PropertyChangedEventHandler PropertyChanged;
+
+        void GetJobItems();
+        void SaveJobItem(JobItem jobItem);
+        void DeleteJobItem(JobItem jobItem);
+    }
+}
---
+++ Timetracker/ViewModels/TimetrackerViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Timetracker.Models;
+
+namespace Timetracker.ViewModels
+{
+    public class TimetrackerViewModel : ITimetrackerViewModel
+    {
+        public TimetrackerViewModel()
+        {
+
+        }
+
+        private bool isBusy = false;
+        public bool IsBusy 
+        {
+            get => isBusy;
+            private set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<JobItem> jobItems = new List<JobItem>();
+        public List<JobItem> JobItems
+        {
+            get => jobItems;
+            private set
+            {
+                jobItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private JobItem jobItem = new JobItem();
+        public JobItem JobItem 
+        { 
+            get => jobItem;
+            set
+            {
+                jobItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public void DeleteJobItem(JobItem jobItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetJobItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveJobItem(JobItem jobItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
