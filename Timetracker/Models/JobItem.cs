b6b525acee5b465ed0ffa363fac64c1f06951692
Steff <steffik369@gmail.com>
2021-03-09 09:56:54 +0100

    feat: Add JobItem model
    Added JobItem model, that represent one row in timetracker table

---
+++ Timetracker/Models/JobItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Timetracker.Models
+{
+    public class JobItem
+    {
+        public string Title { get; set; }
+        public JobType Type { get; set; }
+        public DateTimeOffset Start { get; set; }
+        public DateTimeOffset End { get; set; }
+        public TimeSpan ElapsedTime { get; set; }
+    }
+
+    public enum JobType
+    {
+        Coding,
+        Testing,
+        Debuging
+    }
+}
